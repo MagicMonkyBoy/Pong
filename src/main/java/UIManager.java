4865353f04abe7f8f76b9021713481a5ea5da4b4
MagicMonkyBoy <44240860+MagicMonkyBoy@users.noreply.github.com>
2020-08-06 01:12:12 -0500

    Started working on UIManager and UIObject

---
+++ src/main/java/UIManager.java
@@ -0,0 +1,42 @@
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+public class UIManager {
+
+    Handler handler;
+    private ArrayList<UIObjects> objects;
+
+    public UIManager(Handler handler) {
+        this.handler = handler;
+
+    }
+
+    public void tick() {
+
+    }
+
+    public void render() {
+
+    }
+
+    public void onMouseMove(MouseEvent e) {
+
+    }
+
+    public void onMouseRelease(MouseEvent e) {
+
+    }
+
+    public void addObject() {
+
+    }
+
+    public void removeObject() {
+
+    }
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+}
---
+++ src/main/java/UIObjects.java
@@ -0,0 +1,9 @@
+public abstract class UIObjects {
+
+    public UIObjects() {
+
+    }
+
+
+
+}
